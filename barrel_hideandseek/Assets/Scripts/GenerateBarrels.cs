336f831a5e16d83511ce767a8aab7ef785d243c2
Deafelopment <deafelopment@gmail.com>
2022-11-08 23:10:47 +0000

    Created barrel spawn logic

---
+++ barrel_hideandseek/Assets/Scripts/GenerateBarrels.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateBarrels : MonoBehaviour
+{
+
+    public GameObject theBarrel;
+    public int xPos;
+    public int zPos;
+    public int barrelCount;
+
+    void Start()
+    {
+        StartCoroutine(BarrelDrop());
+    }
+
+    IEnumerator BarrelDrop()
+    {
+        while (barrelCount < 10)
+        {
+            xPos = Random.Range(1, 50);
+            zPos = Random.Range(1, 50);
+            Instantiate(theBarrel, new Vector3(xPos, 1, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            barrelCount += 1;
+        }
+    }
+
+}
