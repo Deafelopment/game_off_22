137ae795331a17440383ca34a1f4aabe135385c9
Deafelopment <deafelopment@gmail.com>
2022-12-01 20:55:18 +0000

    Implemented Quit and Restart functions on game over

---
+++ barrel_hideandseek/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Debug.Log("QUIT");
+            Application.Quit();
+
+        if (Input.GetKeyDown(KeyCode.N))
+            SceneManager.LoadScene(1);
+    }
+}
